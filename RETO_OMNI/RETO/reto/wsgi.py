2f41c77efe167dd88f3c176b7b5cfb4f1468791b
camilo <quinterocamilo771@gmail.com>
2021-07-10 22:01:48 -0500

    Montaje de Docker y DockerCompose

--- RETO_OMNI/RETO/reto/wsgi.py
+++ RETO_OMNI/RETO/reto/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RETO project.
+WSGI config for reto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RETO.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reto.settings")
 
 application = get_wsgi_application()
